355728976a420d211b529f7904bcb6047d458567
Aleksander Shumeiko <music.dll@mail.ru>
2020-06-30 13:00:08 +0300

    init

---
+++ tabs.js
@@ -0,0 +1,34 @@
+function tabs(tabMenuHeaderClass, tabMenuItemsClass, tabContentClass) {
+    let tab = document.querySelectorAll('.' + tabMenuItemsClass),
+        info = document.querySelector('.' + tabMenuHeaderClass),
+        tabContent = document.querySelectorAll('.' + tabContentClass);
+
+    function hideTabContent(a) {
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
+    }
+
+    hideTabContent(1);
+
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+    }
+
+    info.addEventListener('click', function(event) {
+        let target = event.target;
+        if (target && target.classList.contains('info-header-tab')) {
+            for (let i = 0; i < tab.length; i++) {
+                if (target == tab[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
+    });
+}
